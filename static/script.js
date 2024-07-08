270331d172a23ec07f1c84a3cee131826299f060
tulio <tuliomgui@hotmail.com>
2024-07-08 12:53:31 -0300

    Making start / stop containers action really work with visual indication of status change

---
+++ static/script.js
@@ -0,0 +1,12 @@
+function updateContainerStatus(evt) {
+    if (evt.detail.successful) {
+        //alert("Requested: Status=" + evt.detail.xhr.status + " / ID=" + evt.detail.elt.getAttribute("data-container-id"));
+        let imgStatus = document.getElementById("status-img-" + evt.detail.elt.getAttribute("data-container-id"));
+        if (imgStatus.src.indexOf("red") < 0)
+            imgStatus.src = imgStatus.src.replace("green.svg","red.svg");
+        else
+            imgStatus.src = imgStatus.src.replace("red.svg","green.svg");
+    } else {
+        alert("Error");
+    }
+}
