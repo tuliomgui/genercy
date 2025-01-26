8e3628a51a212f1d85a9b67db62ea160948d2698
Tulio Guimaraes <tuliomgui@hotmail.com>
2025-01-26 18:24:17 +0000

    Criação dos toasts para enviar mensagens aos usuários

--- static/script.js
+++ static/script.js
@@ -11,15 +11,63 @@
 //     }
 // }
 
+const toastContainer = document.getElementById('toast-container');
+
 function containerResponse(event) {
     if (event.detail.successful) {
         let jsonResp = JSON.parse(event.detail.xhr.response)
-        let success = jsonResp.success;
-        let message = jsonResp.message;
-        console.log("Success: %o", success);
-        console.log("Message: %o", message);
+        addToast(jsonResp.message, jsonResp.success ? 'success' : 'error');
     } else {
-        alert("Não foi possível executar a ação desejada, ocorreu um erro de comunicação com o servidor.");
+        addToast("There was an error with the server communication.", 'error');
     }
-    //console.log("ResponseText: %o", event.detail.xhr.responseText);
+}
+
+function addToast(message, severity = 'info') {
+    if (toastContainer === null) {
+        alert("toastContainer is null");
+        return;
+    }
+    const toastModel = toastContainer.getElementsByClassName('toast-model');
+    if (toastModel === null || toastModel.length === 0) {
+        alert("toastModel is null");
+        return;
+    }
+    const toastClone = toastModel[0].cloneNode(true);
+    toastClone.classList.remove('toast-model');
+    toastClone.classList.add(getSeverityClass(severity));
+    // toastClone.getElementsByTagName('i')[0].classList.add(getSeverityIcon(severity));
+    // toastClone.querySelector('.toast-header').textContent = severity.charAt(0).toUpperCase() + severity.slice(1);
+    toastClone.querySelector('.toast-body').textContent = message;
+    toastContainer.appendChild(toastClone);
+    const toast = new bootstrap.Toast(toastClone);
+    toast.show();
+    toastClone.addEventListener('hidden.bs.toast', function () {
+        toastContainer.removeChild(toastClone);
+    });
+}
+
+function getSeverityClass(severity) {
+    switch (severity) {
+        case 'success':
+            return 'bg-success';
+        case 'warning':
+            return 'bg-warning';
+        case 'error':
+            return 'bg-danger';
+        default:
+            return 'bg-secondary';
+    }
+}
+
+function getSeverityIcon(severity) {
+    switch (severity) {
+        case 'success':
+            return ['bi', 'bi-check'];
+        case 'warning':
+            return ['bi', 'bi-exclamation-triangle'];
+        case 'error':
+            return ['bi', 'bi-x-circle'];
+        default:
+            return ['bi', 'bi-card-checklist'];
+    }
 }
